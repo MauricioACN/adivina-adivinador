a826f6418630d5615952533729da74b65b97d0ab
Alejandro Cano <alejandrocanomn@gmail.com>
2024-04-29 01:37:11 -0400

    move to python 3.11

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,7 @@ config = rx.Config(
     app_name="adivina_adivinador",
     cors_allow_origins=[
         "http://localhost:3000",
-        "https://juego.alejocano.com",
-        "https://adivina-adivinador.vercel.app"
+        # "https://juego.alejocano.com",
+        # "https://adivina-adivinador.vercel.app"
     ],
 )
