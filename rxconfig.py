2397f93c65f9c047cf5d672da930891f6779126e
Alejandro Cano <alejandrocanomn@gmail.com>
2024-04-29 00:20:14 -0400

    cors y api url corregida

--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="adivina_adivinador",
+    cors_allow_origins=[
+        "http://localhost:3000",
+        "https://alejocano.com",
+    ],
 )
