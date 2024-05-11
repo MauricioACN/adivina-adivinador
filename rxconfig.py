2e1a44140cb76f212e90b92233cbe4868d75f678
Alejandro Cano <alejandrocanomn@gmail.com>
2024-05-10 21:27:33 -0400

    app_url to external endpoint

--- rxconfig.py
+++ rxconfig.py
@@ -2,9 +2,10 @@ import reflex as rx
 
 config = rx.Config(
     app_name="adivina_adivinador",
+    api_url="http://localhost:8000",
     cors_allow_origins=[
         "http://localhost:3000",
-        # "https://juego.alejocano.com",
-        # "https://adivina-adivinador.vercel.app"
+        "https://juego.alejocano.com",
+        "https://adivina-adivinador.vercel.app"
     ],
 )
