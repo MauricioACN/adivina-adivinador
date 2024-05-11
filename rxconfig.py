63770633fc2f9151c4ecfa3c44344ed2b69025a5
Alejandro Cano <alejandrocanomn@gmail.com>
2024-05-10 21:39:18 -0400

    app_url apuntando a mi dominio

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="adivina_adivinador",
-    api_url="http://localhost:8000",
+    api_url="api-juego.alejocano.com",
     cors_allow_origins=[
         "http://localhost:3000",
         "https://juego.alejocano.com",
